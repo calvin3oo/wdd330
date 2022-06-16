73f5618a7db05674bc595b8c391b951bb813279f
calvin3oo <86371905+calvin3oo@users.noreply.github.com>
2022-06-15 23:19:35 -0600

    L03 completed

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,7 +3,7 @@ function getLocalStorage(key) {
 }
 
 function getCartContents() {
-  let markup = '';
+  //let markup = '';
   const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => renderCartItem(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
--- src/js/productDetails.js
+++ src/js/productDetails.js
@@ -13,10 +13,12 @@ export default class productData{
         // once we have the product details we can render out the HTML
         // once the HTML is rendered we can add a listener to Add to Cart button
         // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-        document.getElementById('addToCart')
-                .addEventListener('click', this.addToCart.bind(this));
         
         this.product = await this.dataSource.findProductById(this.productId)
+        document.getElementById('tent-container').innerHTML = this.renderProductDetails();
+
+        document.getElementById('addToCart')
+                .addEventListener('click', this.addToCart.bind(this));
       }
     constructor(productId, dataSource){
         this.productId = productId;
@@ -31,7 +33,7 @@ export default class productData{
         <h2 class="divider">${this.product.NameWithoutBrand}</h2>
         <img
           class="divider"
-          src="${this.product.Image}"
+          src="../${this.product.Image}"
           alt="${this.product.NameWithoutBrand}"
         />
         <p class="product-card__price">$${this.product.FinalPrice}</p>
